da81b97ae2288b549147b998907f462ba7ef8e36
Ana Rocabado <anarocabmar@gmail.com>
2022-08-11 20:37:47 -0400

    warlus operator

--- c1_streamlitBegin.py
+++ c1_streamlitBegin.py
@@ -1,5 +1,32 @@
 import streamlit as st
 
 st.write("""
-# Hello world
+# 10 Cool Beginner Python Tricks That Will Make Your Life Easier
+""")
+
+st.write("""
+## 1. Walrus operator
+
+
+The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+
+
+If we want to check and print the length of a list:
+
+
+*Example*
+
+```python
+Mylist = [1,2,3]
+if(l := len(mylist) > 2)
+print(l)
+```
+
+*Output*
+
+```python
+3
+```
+
+
 """)
